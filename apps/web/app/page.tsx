b94fb6fd4e54325ae3d28ac56793d308405c2a4d
Abhishek Jaiswar <abhishekndxw@gmail.com>
2026-07-07 01:40:52 +0530

    feat(convex): complete setup for convex backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@workspace/ui/components/button"
+"use client"
+
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/api"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <h1>app/web</h1>
+
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   )
